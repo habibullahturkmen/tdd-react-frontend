efe937db64c4c6c3e3a97bf3d1512b1480bd8002
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-05-10 19:58:57 +0430

    Interactions test: enables the button when password and password repeat fields have same value: green

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,6 +1,29 @@
 import React from "react";
 
 class SignUpPage extends React.Component {
+
+    state = {
+        disabled: true,
+        password: '',
+        passwordRepeat: ''
+    }
+
+    onChangePassword = (event) => {
+        const currentValue = event.target.value;
+        this.setState({
+            password: currentValue,
+            disabled: currentValue !== this.state.passwordRepeat
+        });
+    }
+
+    onChangePasswordRepeat = (event) => {
+        const currentValue = event.target.value;
+        this.setState({
+            passwordRepeat: currentValue,
+            disabled: currentValue !== this.state.password
+        });
+    }
+
     render() {
         return (
             <div>
@@ -10,10 +33,10 @@ class SignUpPage extends React.Component {
                 <label htmlFor="email">E-mail</label>
                 <input id="email" />
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password" />
+                <input id="password" type="password" onChange={this.onChangePassword} />
                 <label htmlFor="password-repeat">Password Repeat</label>
-                <input id="password-repeat" type="password" />
-                <button disabled>Sign Up</button>
+                <input id="password-repeat" type="password" onChange={this.onChangePasswordRepeat} />
+                <button disabled={this.state.disabled}>Sign Up</button>
             </div>
         );
     }

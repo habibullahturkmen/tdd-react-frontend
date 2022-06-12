474a28565d32108c5c5011027238d4a47e4f9b6d
habibullahturkmen <habibullah_turkmen@yahoo.com>
2022-06-12 11:20:48 +0430

    e2e fixed && navigation tests added for LoginPage and HomePage

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,8 +1,18 @@
 /* eslint-disable no-undef */
-describe('Navigation', () => {
-    it('should find the heading', () => {
-      cy.visit('/')
-  
-      cy.get('h1').contains('Sign Up')
-    })
-  })
+describe("Navigation", () => {
+
+	it("should find the HomePage heading", () => {
+		cy.visit("/");
+		cy.get("h1").contains("Home Page");
+	});
+
+	it("should find the SignUpPage heading", () => {
+		cy.visit("/signup");
+		cy.get("h1").contains("Sign Up");
+	});
+
+	it("should find the LoginPage heading", () => {
+		cy.visit("/login");
+		cy.get("h1").contains("Login Page");
+	});
+})
